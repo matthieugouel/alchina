1d64ee7183eeed631fde7dc6347180ce199120a8
matthieu <matthieu.gouel@gmail.com>
2019-04-19 00:32:36 +0200

    feat(classifiers): implement basic logistic regression

--- tests/test_regressors.py
+++ tests/test_regressors.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from alchina.regressors import LinearRegressor
+from alchina.regressors import LinearRegressor, LogisticRegressor
 
 
 def test_linear_regressor():
@@ -38,5 +38,29 @@ def test_linear_regressor_normal():
     y = np.array([[1]])
 
     lr.normal(X, y)
+
+    assert lr.score(X, y) == 1
+
+
+def test_logistic_regressor():
+    """Test of `LogisticRegressor` class."""
+    lr = LogisticRegressor(learning_rate=0.1, iterations=1, standardize=False)
+
+    X = np.array([[1]])
+    y = np.array([[1]])
+
+    lr.fit(X, y)
+
+    assert lr.score(X, y) == 1
+
+
+def test_logistic_regressor_standardized():
+    """Test of `LogisticRegressor` class with standardization."""
+    lr = LogisticRegressor(learning_rate=0.1, iterations=1, standardize=True)
+
+    X = np.array([[1]])
+    y = np.array([[1]])
+
+    lr.fit(X, y)
 
     assert lr.score(X, y) == 1
