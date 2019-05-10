20a144ae4a412d02aefbaa77763e01af89a5035a
matthieu <matthieu.gouel@gmail.com>
2019-05-10 08:38:54 +0200

    refactor(examples): selection is now a classification with metrics

--- examples/selection.py
+++ examples/selection.py
@@ -1,47 +1,65 @@
-"""Example of algorithm selection."""
+"""Example of algorithm selection for multiclass classification."""
 
 import pandas as pd
 
-from alchina.regressors import LinearRegressor
+from alchina.classifiers import LinearClassifier
+from alchina.metrics import precision_score, recall_score, f1_score
 from alchina.selection import split_dataset
+from alchina.utils import target_reshape
 
 from sklearn import datasets
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
 from timeit import default_timer as timer
 
 
-# Load the Boston house prices dataset
-houses = datasets.load_boston()
+def train_and_report(model, X_train, y_train, X_test, y_test):
+    """Train the model and print a report."""
 
-# Create the input features and target data frame
-df = pd.DataFrame(houses.data, columns=houses.feature_names)
-df["target"] = pd.Series(houses.target)
+    # Training
+    start = timer()
+    model.fit(X_train, y_train)
+    end = timer()
 
-# Select the input features
-X = df[houses.feature_names].values
+    # Report
+    print(f"Accuracy : {model.score(X_test, y_test):.3f}")
+    print("---")
+    precision = precision_score(model.predict(X_test), y_test, average="macro")
+    print(f"Precision : {precision:.3f}")
+    recall = recall_score(model.predict(X_test), y_test, average="macro")
+    print(f"Recall : {recall:.3f}")
+    print("---")
+    f1 = f1_score(model.predict(X_test), y_test, average="macro")
+    print(f"F1 score : {f1:.3f}")
+    print("---")
+    print(f"Training time : {end - start:.4f}s\n")
 
-# Select the target
-y = df["target"]
-y = y.values.reshape(y.shape[0], 1)
 
-X_train, y_train, X_test, y_test = split_dataset(X, y)
+if __name__ == "__main__":
+    # Load the Boston house prices dataset
+    iris = datasets.load_iris()
 
-# Alchina regression
-print("--- Alchina ---")
-start = timer()
-lr_alchina = LinearRegressor(learning_rate=0.03, iterations=1000, standardize=True)
-lr_alchina.fit(X_train, y_train)
-end = timer()
+    # Create the input features and target data frame
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df["target"] = pd.Series(iris.target)
 
-print(f"Regression score : {lr_alchina.score(X_test, y_test):.3f}")
-print(f"Computation time : {end - start:.4f}s")
+    # Select the input features
+    X = df[iris.feature_names].values
 
-# Scikit-learn regression
-print("--- Scikit-learn ---")
-start = timer()
-lr_skitlearn = LinearRegression(normalize=True)
-lr_skitlearn.fit(X_train, y_train)
-end = timer()
+    # Select the target
+    y = df["target"]
+    y = y.values.reshape(y.shape[0], 1)
 
-print(f"Regression score : {lr_skitlearn.score(X_test, y_test):.3f}")
-print(f"Computation time : {end - start:.4f}s")
+    X_train, y_train, X_test, y_test = split_dataset(X, y)
+
+    # Alchina regression
+    print("--- Alchina ---")
+    lr_alchina = LinearClassifier(learning_rate=0.05, iterations=2000, standardize=True)
+    train_and_report(lr_alchina, X_train, y_train, X_test, y_test)
+
+    # Scikit-learn regression
+    print("--- Scikit-learn ---")
+    lr_skitlearn = LogisticRegression(solver="liblinear", multi_class="ovr")
+    train_and_report(
+        lr_skitlearn, X_train, target_reshape(y_train), X_test, target_reshape(y_test)
+    )
